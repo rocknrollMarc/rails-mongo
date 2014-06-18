7ed3a074e21595da61d861eabf7d236eeef29fe8
Marc Blümner <m.bluemner@liquidlabs.de>
2014-06-18 15:41:47 +0200

    rails_apps_composer: rspec files

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,12 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe HomeController, :type => :controller do
+describe HomeController do
+
+  describe "GET 'index'" do
+    it "returns http success" do
+      get 'index'
+      response.should be_success
+    end
+  end
 
 end
